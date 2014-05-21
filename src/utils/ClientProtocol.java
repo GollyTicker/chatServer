19527980c7e86d043306c057e71ee7156d77daf8
GollyTicker <golly.ticker@gmail.com>
2014-05-21 09:23:09 +0200

    added basic protocols

--- src/utils/ClientProtocol.java
+++ src/utils/ClientProtocol.java
@@ -1,7 +1,32 @@
 package utils;
 
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.util.List;
+
 /**
  * Created by Swaneet on 20.05.2014.
  */
 public class ClientProtocol {
+
+    public static final int UDP_PORT = 50000;
+
+    public static final String INFO = "INFO";
+    public static final String BYE = "BYE";
+    public static final String NEW = "NEW";
+    public static final String NEWLINE = "\r\n";
+    public static final String SPACE = " ";
+
+    public static String new_(String userName) {
+        return NEW + SPACE + userName;
+    }
+
+    public static String info() {
+        return INFO;
+    }
+
+    public static String bye() {
+        return BYE;
+    }
+
 }
--- src/utils/ServerProtocol.java
+++ src/utils/ServerProtocol.java
@@ -1,7 +1,40 @@
 package utils;
 
+import java.util.List;
+
 /**
  * Created by Swaneet on 20.05.2014.
  */
 public class ServerProtocol {
+
+    public static final int TCP_PORT = 50000;
+
+    public static final String TCP_HOSTNAME = "localhost";
+
+    public static final String OK = "OK";
+    public static final String ERROR = "ERROR";
+
+    public static final String BYE = "BYE";
+    public static final String LIST = "LIST";
+    public static final String NEWLINE = "\r\n";
+    public static final String SPACE = " ";
+
+    public static String list(List<User> ls) {
+        return LIST + SPACE +  "hier die users und dessen hosts";
+    }
+
+    public static String ok() {
+        return OK;
+    }
+
+    public static String error(String message) {
+        return (ERROR + SPACE + message);
+    }
+
+    public static String bye() {
+        return BYE;
+    }
+
+
+
 }
