336f1bb7141db285fa6069f3a0abe3d2c4249162
GollyTicker <golly.ticker@gmail.com>
2014-05-21 09:43:51 +0200

    adding basic threading for new clients

--- src/Server/ServerMain.java
+++ src/Server/ServerMain.java
@@ -1,10 +1,49 @@
 package server;
 
+import utils.ServerProtocol;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by Swaneet on 20.05.2014.
  */
 public class ServerMain {
 
-    // server
-    // ServerThread
+    // ReadFcWriteFs stream;
+    static ServerSocket welcomeSocket;
+
+    public ServerMain() {
+        initializeServer();
+    }
+
+    private void initializeServer() {
+        try {
+            welcomeSocket = new ServerSocket(ServerProtocol.TCP_PORT);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleNewClients() {
+        try {
+            Socket clientSocket = welcomeSocket.accept();
+            new ServerThread(clientSocket).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void serverRun() {
+        while (true) {
+            handleNewClients();
+        }
+    }
+
+    public static void main(String[] args) {
+        ServerMain s = new ServerMain();
+        s.serverRun();
+    }
+
 }
--- src/Server/ServerThread.java
+++ src/Server/ServerThread.java
@@ -1,7 +1,32 @@
 package server;
 
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Created by Swaneet on 20.05.2014.
  */
-public class ServerThread {
+public class ServerThread extends Thread {
+    Socket socket;
+    BufferedReader reader;
+    PrintWriter writer;
+
+    public ServerThread(Socket clientSocket) {
+        socket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        initReaderWriter();
+    }
+
+    private void initReaderWriter() {
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            writer = new PrintWriter(
+                    new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+        }
+    }
 }
