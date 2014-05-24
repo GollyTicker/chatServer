a2f503de76d22bc8b0b59730e1ce654d4026dc19
GollyTicker <golly.ticker@gmail.com>
2014-05-24 15:35:40 +0200

    wrote ChatMsg class

--- src/Client/ChatMsg.java
+++ src/Client/ChatMsg.java
@@ -1,11 +1,33 @@
 package Client;
 
+import utils.ClientProtocol;
+import utils.ServerProtocol;
+
 /**
  * Created by Swaneet on 20.05.2014.
  */
 public class ChatMsg {
     // stellt eine einzige Chat Zeile dar.
 
-    // String name
-    // String msg
+    public String name;
+    public String msg;
+
+    public ChatMsg(String name, String rawMsg) {
+        this.name = name;
+        String singleLineMsg = rawMsg.replace('\n', ' ');  // darf keine NEWLINES enthalten
+        String finalMsg = "";
+        // darf hoechstens 100 Zeichen lang sein.
+        if(singleLineMsg.length() < 100) {
+            finalMsg = singleLineMsg;
+        }
+        else {
+            finalMsg = singleLineMsg.substring(0,99);
+        }
+        this.msg = finalMsg;
+    }
+
+    @Override
+    public String toString() {
+        return name  + ClientProtocol.CHAT_MSG_SEPERATOR + ServerProtocol.SPACE + msg;
+    }
 }
