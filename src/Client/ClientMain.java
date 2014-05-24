61e9d688343c8b31deb4f967a07408a70639c188
Allquantor <morozovwin@googlemail.com>
2014-05-24 13:35:17 +0200

    first approach for the guti implementation before tsting

--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -1,6 +1,6 @@
 package Client;
 
-import Client.GUI.GUIThread;
+import Client.GUI.ClientGUI;
 import Client.ThreadSafeData.Storage;
 import utils.ClientProtocol;
 import utils.ServerProtocol;
@@ -14,16 +14,17 @@ public class ClientMain {
 
         Storage storage = new Storage();    // thread-safe Storage anschmeißen
 
-        GUIThread guiThread = new GUIThread(storage);   // die GUI hat Zugriff auf die Daten
+        //GUIThread guiThread = new GUIThread(storage);
+        ClientGUI gui = new ClientGUI(storage);  // die GUI hat Zugriff auf die Daten
 
         // Der TCP Thread hat Zugriff auf die Daten und auf Services der GUI
-        TCPThread tcp = new TCPThread(ServerProtocol.TCP_HOSTNAME, ServerProtocol.TCP_PORT, guiThread, storage);
+        TCPThread tcp = new TCPThread(ServerProtocol.TCP_HOSTNAME, ServerProtocol.TCP_PORT, gui, storage);
 
         // Der UDP thread
         UDPThread udp = new UDPThread(ClientProtocol.UDP_PORT, storage);
 
         // starten aller Threads
-        guiThread.start();
+        //guiThread.start();
         tcp.start();
         udp.start();
     }
---
+++ src/Client/GUI/ClientGUI.java
@@ -0,0 +1,132 @@
+package Client.GUI;
+
+import Client.ThreadSafeData.StorageServices;
+import utils.ServerProtocol;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+/*
+ * The Client with its GUI
+ */
+public class ClientGUI extends JFrame implements ActionListener, GUIServices {
+
+
+    // laber for the Username field description
+    private JLabel userNameLabel;
+    //testfield for enter the username
+    private JTextField userNameTextField;
+    //buttons
+    private JButton login, sendMessage;
+
+    private volatile String userNameStr = "";
+
+    // for the chat room
+    private JTextArea chatTextArea;
+
+    JPanel loginPanel;
+
+    private StorageServices storageServices;
+
+    public ClientGUI(StorageServices storageServices) {
+        super("Chabo Chat");
+
+        this.storageServices = storageServices;
+
+
+        //
+        userNameLabel = new JLabel("Username", SwingConstants.CENTER);
+
+        //default value
+        userNameTextField = new JTextField("");
+
+        userNameTextField.setBackground(Color.BLUE);
+
+        // login button
+        login = new JButton("Login");
+        login.addActionListener(this);
+
+
+        loginPanel = new JPanel();
+        loginPanel.add(userNameLabel, userNameTextField);
+        loginPanel.add(login);
+        add(loginPanel, BorderLayout.CENTER);
+
+        // the chat room
+        // switchToChatMode();
+
+        userNameTextField.addActionListener(this);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600, 600);
+        setVisible(true);
+        userNameTextField.requestFocus();
+    }
+
+    private void switchToChatMode() {
+
+        //set login panel invisibles
+        loginPanel.removeAll();
+        loginPanel.setVisible(false);
+
+        chatTextArea = new JTextArea("=== Connected ===\n", 80, 80);
+        chatTextArea.setVisible(false);
+        JPanel centerPanel = new JPanel(new GridLayout(1, 1));
+        chatTextArea.setEditable(false);
+
+        sendMessage = new JButton("Send");
+        sendMessage.setVisible(false);
+        sendMessage.addActionListener(this);
+
+        centerPanel.add(sendMessage);
+        centerPanel.add(new JScrollPane(chatTextArea));
+        add(centerPanel, BorderLayout.CENTER);
+    }
+
+    //
+    void append(String str) {
+        chatTextArea.append(str);
+        chatTextArea.setCaretPosition(chatTextArea.getText().length() - 1);
+    }
+
+
+    public void actionPerformed(ActionEvent e) {
+        Object o = e.getSource();
+        if (o == login) {
+            userNameStr = userNameTextField.getText();
+            notifyAll();
+            return;
+        }
+        if (o == sendMessage) {
+            System.out.println("Not implemented!");
+            return;
+        }
+    }
+
+    @Override
+    public synchronized String getUserName() {
+        try {
+            while(userNameStr.equals(""))
+                wait();
+            String user = new String(userNameStr);
+            userNameStr = "";
+            return user;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    @Override
+    public void nameRegistrationResponse(String msg) {
+        if(msg.equals(ServerProtocol.OK)) {
+            switchToChatMode(); // if registration successful, then switch to chat mode
+        }
+        else {
+            userNameLabel.setText(msg); // else display the error
+        }
+    }
+}
