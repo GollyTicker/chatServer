0fdfe49f86f4314ee5d0bbad3212a47113d1f86c
GollyTicker <golly.ticker@gmail.com>
2014-05-24 15:36:16 +0200

    sample chat messages in client. dummy for UDP thread

--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -20,13 +20,27 @@ public class ClientMain {
         // Der TCP Thread hat Zugriff auf die Daten und auf Services der GUI
         TCPThread tcp = new TCPThread(ServerProtocol.TCP_HOSTNAME, ServerProtocol.TCP_PORT, gui, storage);
 
-        // Der UDP thread
-        UDPThread udp = new UDPThread(ClientProtocol.UDP_PORT, storage);
+        // Der UDP thread. UDP Thread needs to tell the GUI to get its messages.
+        UDPThread udp = new UDPThread(ClientProtocol.UDP_PORT, /*gui,*/ storage);
 
         // starten aller Threads
         //guiThread.start();
         tcp.start();
         udp.start();
+
+        // Some Sample ChatMessages
+        /*try {
+            Thread.sleep(2500);
+            storage.addChatMessage(new ChatMsg("Superman", "Hello World'! '' sdf :D"));
+            gui.refreshChatMessages();
+            Thread.sleep(500);
+            storage.addChatMessage(new ChatMsg("IvanMorozov", "This is my second staement"));
+            gui.refreshChatMessages();
+            Thread.sleep(500);
+            storage.addChatMessage(new ChatMsg("SwaneetSahoo", "Third statement!!"));
+            gui.refreshChatMessages();
+        } catch (InterruptedException e) {
+        }*/
     }
 
 }
